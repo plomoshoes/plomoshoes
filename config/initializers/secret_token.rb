4f558ae153295443eba85d15e09c31d1510de599
Leandro Alvares da Costa <leandroadacosta@gmail.com>
2012-09-11 23:02:54 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plomoshoes::Application.config.secret_token = 'd4657f60bbd7c368b809e42ef8ff169e964afd677cd5c4bd429543d0efe4b8ade50c8a68d4c75aa24618a87701198e165821ffa55969669f4ba4aa1aa39d677a'
