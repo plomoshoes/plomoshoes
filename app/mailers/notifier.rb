68f60ac7bc01f0c2702def1f09e74ebc19c45f6a
Leandro Alvares da Costa <leandroadacosta@gmail.com>
2012-09-25 22:36:03 -0300

    ajustes no html do email de buy notification

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,9 +1,11 @@
 class Notifier < ActionMailer::Base
   
+  default :from => 'website@plomoshoes.com'
+  
   def buy_notification(shoe, email)
     @shoe = shoe
     @email = email
-    mail :from => 'site@plomoshoes.com', :to => 'info@plomoshoes.com', :subject => "[Site] Buy notification of #{@email}"
+    mail :to => 'info@plomoshoes.com', :subject => "[website] Buy notification of #{@email}"
   end
   
 end
