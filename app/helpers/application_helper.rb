252a94b3d63837fda4200a3aa18b17d628989c4c
Leandro Alvares da Costa <leandroadacosta@gmail.com>
2012-10-18 11:21:35 -0300

    implementado aparecer link para todas collections no menu quando nao tiver nenhuma collection marcada como current

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,15 @@ module ApplicationHelper
     Collection.current
   end
   
+  def current_collection_content_tag
+    if current_collection.nil?
+      link_tag = link_to 'COLLECTIONS', collections_url
+    else
+      link_tag = link_to current_collection.name, collection_url(current_collection.path)
+    end
+    content_tag :li, link_tag, :class => selected_menu_class(['collections', 'shoes'])
+  end
+  
   def asset_url(asset)
     "#{request.protocol}#{request.host_with_port}#{asset_path(asset)}"
   end
